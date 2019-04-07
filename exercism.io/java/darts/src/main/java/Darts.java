76c76b406a7b99bc2652639e3104d0a1dd3ec64f
Melon Bread <rain@melonbread.dev>
2019-04-07 18:13:03 -0400

    Finished
    Circles are not squares

--- exercism.io/java/darts/src/main/java/Darts.java
+++ exercism.io/java/darts/src/main/java/Darts.java
@@ -1,11 +1,31 @@
+import java.awt.geom.Point2D;
+
 class Darts {
 
+    private double x, y;
+
     Darts(double x, double y) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        this.x = x;
+        this.y = y;
     }
 
     int score() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+        double distance = Point2D.distance(0, 0, x, y);
 
+        // The dart goes outside the board
+        if (distance > 10)
+            return 0;
+
+        // The dart lands within the outer circle
+        else if (distance > 5)
+            return 1;
+
+        // The dart lands within the middle circle
+        else if (distance > 1)
+            return 5;
+
+        // The dart lands dead center
+        else
+            return 10;
+    }
 }

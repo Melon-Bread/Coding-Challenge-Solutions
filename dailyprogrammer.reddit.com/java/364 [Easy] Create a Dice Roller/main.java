03330b92f3fa75e9bbf3e2cbf6f2658c4b4f2194
Melon Bread <rain@melonbread.dev>
2019-04-07 18:18:10 -0400

    Started

---
+++ dailyprogrammer.reddit.com/java/364 [Easy] Create a Dice Roller/main.java
@@ -0,0 +1,8 @@
+// [2018-06-18] Challenge #364 [Easy] Create a Dice Roller
+// https://www.reddit.com/r/dailyprogrammer/comments/8s0cy1/20180618_challenge_364_easy_create_a_dice_roller/
+
+public class Main {
+	public static void main(String[] args) {
+		// Code goes here
+	}
+}

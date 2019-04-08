d833ce8fc56ae45246c8ea6c76952817ba789923
Melon Bread <rain@melonbread.dev>
2019-04-08 01:44:52 -0400

    Finshed
    Should have used a 'switch'  statement instead of 'if else' staments could have gotten cleaner lines.
    ```case 'G: rnaStrand.append('C'); break;```

--- exercism.io/java/rna-transcription/src/main/java/RnaTranscription.java
+++ exercism.io/java/rna-transcription/src/main/java/RnaTranscription.java
@@ -1,7 +1,18 @@
 class RnaTranscription {
-
     String transcribe(String dnaStrand) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+        StringBuilder rnaStrand = new StringBuilder();
 
+        for (int i = 0; i < dnaStrand.length(); i++) {
+            if (dnaStrand.charAt(i) == 'G')
+                rnaStrand.append('C');
+            else if(dnaStrand.charAt(i) == 'C')
+                rnaStrand.append('G');
+            else if(dnaStrand.charAt(i) == 'T')
+                rnaStrand.append('A');
+            else if(dnaStrand.charAt(i) == 'A')
+                rnaStrand.append('U');
+        }
+
+        return rnaStrand.toString();
+    }
 }

ecee700d56c2aa25cdf24845edb85568f9171502
Melon Bread <rain@melonbread.dev>
2019-04-07 20:39:11 -0400

    Update Main.java

---
+++ dailyprogrammer.reddit.com/java/364 [Easy] Create a Dice Roller/Main.java
@@ -0,0 +1,49 @@
+// [2018-06-18] Challenge #364 [Easy] Create a Dice Roller
+// https://www.reddit.com/r/dailyprogrammer/comments/8s0cy1/20180618_challenge_364_easy_create_a_dice_roller/
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class DiceRoll {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        String diceRoll;
+        int diceCount, diceSides, diceTotal;
+
+        while (diceRoll != "exit") {
+            String beforeD, afterD;
+            int locationD;
+
+            System.out.println("Enter your dice roll:");
+            diceRoll = scanner.next();
+
+            for (int i = 0; i < diceRoll.length(); i++) {
+                char c = diceRoll.charAt(i);
+                if (c == 'd' || c == 'D')
+                    locationD = i;
+            }
+
+            for (int i = 0; i < diceRoll.length(); i++) {
+                char c = diceRoll.charAt(i);
+
+                if (c != 'd' || c != 'D') {
+                    if (i < locationD)
+                        beforeD += c;
+                    else if (i > locationD)
+                        afterD += c;
+                }
+            }
+
+            diceCount = Integer.parseInt(beforeD);
+            diceSides = Integer.parseInt(afterD);
+
+            for (int i = 0; i < diceCount; i++) {
+                diceTotal += random.nextInt(diceSides) + 1;
+            }
+
+            System.out.println("The total of your dice roll is: " + diceTotal);
+        }
+    }
+}

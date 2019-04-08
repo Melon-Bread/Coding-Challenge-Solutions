3ce74dc38dc04ab18a57d5b02ae6c47f3166e6bd
Melon Bread <rain@melonbread.dev>
2019-04-08 00:59:09 -0400

    Finished
    .charAt() is nice

--- exercism.io/java/armstrong-numbers/src/main/java/ArmstrongNumbers.java
+++ exercism.io/java/armstrong-numbers/src/main/java/ArmstrongNumbers.java
@@ -1,9 +1,13 @@
 class ArmstrongNumbers {
-
 	boolean isArmstrongNumber(int numberToCheck) {
+		int[] digits = new int[Integer.toString(numberToCheck).length()];
+		int digitTotal = 0;
 
-		throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-		
+        for (int i = 0; i < digits.length; i++) {
+            digits[i] = Character.getNumericValue(Integer.toString(numberToCheck).charAt(i));
+            digitTotal += (int)Math.pow(digits[i], digits.length);
+        }
+
+        return digitTotal == numberToCheck;
 	}
-
 }
